7af2301dffbfe6503c223c73f6694158cbf7f61b
F1ner <123871318+F1ner@users.noreply.github.com>
2023-12-11 15:49:03 +0200

    123

---
+++ AbabiySiglov/Figures.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbabiySiglov
+{
+    internal class Figures
+    {
+        public string name;
+        
+        public int side1, side2, side3;
+        public Figures() { }
+        public Figures(string name, int side1, int side2)
+        { 
+            this.name = name;
+            this.side1 = side1;
+            this.side2 = side2;
+            
+        }
+        public override string ToString() { return $"name-{name}\t side1-{side1}\t side2-{side2}\t side3-{side3}"; }
+       
+        public void PS()
+        {
+            int perimetre = (side1 + side2) * 2;
+            
+            int square = side1 * side2;
+            Console.WriteLine($"per={perimetre}, sq={square}");
+        }
+        public void PS1()
+        {
+            int perimetre = side1 + side2 + side3;
+            double p = perimetre / 2;
+            double square = Math.Sqrt(p*(p-side1)*(p-side2)*(p-side3));
+            Console.WriteLine($"per={perimetre}, sq={square}");
+        }
+        public Figures(string name, int side1, int side2, int side3) : this(name, side1, side2)
+        {
+            this.side3 = side3;
+        }
+    }
+}
---
+++ AbabiySiglov/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbabiySiglov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Figures figure1 = new Figures();
+            figure1.name = "kvadr";
+            figure1.side1 = 1;
+            figure1.side2 = 2;
+            Console.WriteLine(figure1.ToString());
+            figure1.PS();
+            Figures figure2 = new Figures("kv",12,10);
+            Console.WriteLine(figure2.ToString());
+            figure2.PS();
+            Console.WriteLine("Введите название фигуры");
+            string name=Console.ReadLine();
+            Console.WriteLine("Введите сторону 1");
+            int s1=int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Введите сторону 2");
+            int s2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите сторону 3");
+            int s3 = int.Parse(Console.ReadLine());
+            Figures figures3= new Figures(name,s1,s2,s3);
+            Console.WriteLine(figures3.ToString());
+            figures3.PS1();
+
+
+            Console.ReadKey();
+        }
+    }
+}
